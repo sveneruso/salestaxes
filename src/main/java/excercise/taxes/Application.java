7a3397023f748585891ac8f37184445fcc3271f9
Samuele Veneruso <samuele.veneruso@gmail.com>
2016-12-09 17:58:20 +0100

    Fixed issue #2

--- src/main/java/excercise/taxes/Application.java
+++ src/main/java/excercise/taxes/Application.java
@@ -3,7 +3,7 @@ package excercise.taxes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
+/*
  * @author sveneruso
  */
 @SpringBootApplication

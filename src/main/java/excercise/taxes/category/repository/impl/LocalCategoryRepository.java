0f3a0bc77099400b59e0669c8275dadd500727c1
Samuele Veneruso <samuele.veneruso@gmail.com>
2016-12-13 12:51:08 +0100

    Fixed issu on LocalCategoryRepository

--- src/main/java/excercise/taxes/category/repository/impl/LocalCategoryRepository.java
+++ src/main/java/excercise/taxes/category/repository/impl/LocalCategoryRepository.java
@@ -10,18 +10,20 @@ import java.util.List;
 import java.util.Map;
 
 @Component
-public class LocalCategoryRepository implements CategoryRepository{
+public class LocalCategoryRepository implements CategoryRepository {
 
-    List<Category> categoryList = new ArrayList<Category>() {{
-        add(new Category("c_001", "Books"));
-        add(new Category("c_002", "Music"));
-        add(new Category("c_003", "Food"));
-        add(new Category("c_004", "Perfume"));
-        add(new Category("c_005", "Medicals"));
-    }};
+    List<Category> categoryList = new ArrayList<Category>() {
+        {
+            add(new Category("c_001", "Books"));
+            add(new Category("c_002", "Music"));
+            add(new Category("c_003", "Food"));
+            add(new Category("c_004", "Perfume"));
+            add(new Category("c_005", "Medicals"));
+        }
+    };
 
-    Map<String, Category> categories =new HashMap<String, Category>(){{
-        for(Category category : categoryList) {
+    Map<String, Category> categories = new HashMap<String, Category>() {{
+        for (Category category : categoryList) {
             put(category.getCode(), category);
         }
     }};
@@ -30,7 +32,7 @@ public class LocalCategoryRepository implements CategoryRepository{
     public Category getCategory(String code) {
         Category category = null;
 
-        if(categories.containsKey(code)) {
+        if (categories.containsKey(code)) {
             category = categories.get(code);
         }
 

1fc4b8880f30859f0f5686fd092bc916244795eb
Samuele Veneruso <samuele.veneruso@gmail.com>
2016-12-13 12:42:19 +0100

    Fixed request mapping on category Controller

--- src/main/java/excercise/taxes/category/controller/CategoryController.java
+++ src/main/java/excercise/taxes/category/controller/CategoryController.java
@@ -5,11 +5,12 @@ import excercise.taxes.category.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.ArrayList;
 import java.util.List;
+
 
 @RestController
 public class CategoryController {
@@ -21,7 +22,7 @@ public class CategoryController {
         return categoryRepository.getCategory(code);
     }
 
-    @RequestMapping()
+    @RequestMapping(value = "/category", method = RequestMethod.GET)
     public List<Category> getCategory() {
         List<Category> response = new ArrayList<>();
 

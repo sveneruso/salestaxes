e490f2fbe5c81a5c201bee709fa1a7e9e3a7a29a
Samuele Veneruso <samuele.veneruso@gmail.com>
2016-12-13 12:57:11 +0100

    Fixed style issues on LocalProductRepository

--- src/main/java/excercise/taxes/product/repository/impl/LocalProductRepository.java
+++ src/main/java/excercise/taxes/product/repository/impl/LocalProductRepository.java
@@ -12,40 +12,42 @@ import java.util.Map;
 @Component
 public class LocalProductRepository implements ProductRepository {
 
-    private List<Product> productList = new ArrayList<Product>() {{
-        // Books category c_001
-        add(new Product("001", "book", "c_001", false));
+    private List<Product> productList = new ArrayList<Product>() {
+        {
+            // Books category c_001
+            add(new Product("001", "book", "c_001", false));
 
-        // Music c_002
-        add(new Product("002", "music CD", "c_002", false));
+            // Music c_002
+            add(new Product("002", "music CD", "c_002", false));
 
-        // Food category c_003
-        add(new Product("003", "chocolate bar", "c_003", false));
-        add(new Product("004", "imported box of chocolates", "c_003", true));
-        add(new Product("008", "box of imported chocolates", "c_003", true));
+            // Food category c_003
+            add(new Product("003", "chocolate bar", "c_003", false));
+            add(new Product("004", "imported box of chocolates", "c_003", true));
+            add(new Product("008", "box of imported chocolates", "c_003", true));
 
-        // Perfume category c_004
-        add(new Product("005", "imported bottle of perfume", "c_004", true));
-        add(new Product("006", "imported bottle of perfume", "c_004", true));
-        add(new Product("007", "bottle of perfume", "c_004", false));
+            // Perfume category c_004
+            add(new Product("005", "imported bottle of perfume", "c_004", true));
+            add(new Product("006", "imported bottle of perfume", "c_004", true));
+            add(new Product("007", "bottle of perfume", "c_004", false));
 
-        // Medicals category c_005
-        add(new Product("009", "packet of headache pills", "c_005", false));
+            // Medicals category c_005
+            add(new Product("009", "packet of headache pills", "c_005", false));
 
-    }};
+        }
+    };
 
     private Map<String, Product> products = new HashMap<String, Product>() {{
         for (Product product : productList) {
             put(product.getCode(), product);
         }
-    }}  ;
+    }};
 
 
     @Override
     public Product getProduct(String code) {
         Product product = null;
 
-        if(products.containsKey(code)) {
+        if (products.containsKey(code)) {
             product = products.get(code);
         }
 
